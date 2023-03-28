f9b16aefec6a7ac49eb717d5ea206c9b66c4ccd0
tildehannevad <hati22fq@student.ju.se>
2023-03-28 13:56:33 +0200

    tellus face

--- tellus.js
+++ tellus.js
@@ -1,7 +1,33 @@
 background(255, 255, 255);
 noStroke();
 
-// tellus
+// tellus face
 fill(139,69,19);
-ellipse(300, 500, 100, 250);
-triangle(250, 380, 350, 380, 300, 300);
+ellipse(300, 300, 150, 110);
+ellipse(300, 350, 60, 20);
+triangle(230, 230, 270, 270, 230, 280);
+triangle(360, 230, 320, 270, 365, 300);
+fill(255, 192, 203);
+triangle(233, 240, 250, 260, 235, 275);
+triangle(355, 270, 340, 260, 355, 240);
+
+// tellus nose
+fill(255, 192, 203);
+ellipse(300, 315, 20, 10);
+fill(0, 0, 0);
+ellipse(295, 318, 3);
+ellipse(305, 318, 3);
+
+//tellus eyes
+ellipse(270, 290, 27, 12);
+ellipse(330, 290, 27, 12);
+fill(255, 255, 255);
+ellipse(270, 290, 25, 10);
+ellipse(330, 290, 25, 10);
+fill(75, 139, 59);
+ellipse(270, 290, 10);
+ellipse(330, 290, 10);
+
+//tellus mouth
+
+
