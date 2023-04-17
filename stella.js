e14655917570a56aee9ddf962779e052a9b3f7e1
Linnea <veli22ts@student.ju.se>
2023-04-17 17:47:23 +0200

    standing stella done

--- stella.js
+++ stella.js
@@ -83,6 +83,7 @@ function sittingStella(x, y, s) {
   pop();
 
   //Nose
+  fill(180, 180, 180);
   triangle(97, 108, 103, 108, 100, 113);
 
   //Mouth
@@ -111,27 +112,78 @@ function sittingStella(x, y, s) {
   pop();
 }
 
-// function standingStella(x, y, s) {
-//   push();
-//   translate(x, y);
-//   scale(s);
-noStroke();
+function standingStella(x, y, s) {
+  push();
+  translate(x, y);
+  scale(s);
+  noStroke();
+  background(250, 250, 250);
 
-//Head
-fill(200, 200, 200);
-ellipse(100, 100, 20, 30);
+  //Head
+  fill(200, 200, 200);
+  ellipse(100, 100, 20, 30);
 
-//Ear
-triangle(95, 88, 104, 75, 105, 95);
+  //Ear
+  fill(200, 200, 200);
+  beginShape();
+  vertex(95, 88);
+  bezierVertex(105, 70, 105, 80, 105, 88);
+  endShape();
 
-//Eye
-fill(106, 136, 168);
-ellipse(107, 98, 5, 6);
-fill(0, 0, 0);
-ellipse(107, 98, 2, 6);
+  fill(221, 171, 155);
+  beginShape();
+  vertex(100, 87);
+  bezierVertex(100, 78, 108, 79, 104, 87);
+  endShape();
 
-//Nose
+  //Nose
+  fill(200, 200, 200);
+  triangle(108, 100, 108, 107, 112, 107);
 
+  //Eye
+  fill(106, 136, 168);
+  ellipse(107, 98, 5, 6);
+  fill(0, 0, 0);
+  ellipse(107, 98, 2, 6);
 
-//Body
-// }
+  //Tail
+  fill(200, 200, 200);
+  stroke(200, 200, 200);
+
+  curve(100, 90, 50, 122, 30, 100, 60, 140);
+  noStroke();
+  //Body
+  fill(200, 200, 200);
+  rect(45, 110, 50, 25, 10);
+
+  //Neck
+  push();
+  translate(90, 104);
+  rotate(95);
+  rect(0, 0, 12, 25);
+  pop();
+
+  //Legs
+  stroke(180, 180, 180);
+  line(54, 135, 54, 150);
+  line(80, 135, 80, 150);
+  line(64, 135, 64, 150);
+  line(90, 133, 90, 150);
+
+  noStroke();
+  rect(54, 130, 10, 20);
+  rect(80, 130, 10, 20);
+
+  //Paws
+  noStroke();
+  fill(180, 180, 180);
+  ellipse(60, 150, 12, 6);
+  ellipse(86, 150, 12, 6);
+
+  //Whiskers
+  stroke(180, 180, 180);
+  strokeWeight(0.5);
+  line(107, 106, 104, 106);
+  line(107, 107, 104, 108);
+  line(107, 105, 104, 104);
+}
